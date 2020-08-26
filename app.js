d8e4ef6c0e52f0fd8bd8fc2281a835e799122657
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2020-08-25 20:13:55 -0700

    fixed entry point for pkg

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const calculateDistance = require('./helper/calculateDistance').calculateDistanc
 const getAllCityDistances = require('./helper/getAllCityDistances').getAllCityDistances;
 const getInfo = require('./helper/getInfo').getInfo;
 
-
-const zipCodeData = {
-    //3 parameters: (zipcode1, zipcode2, optional:units), valid units: 'M','K','N' 
-    zipCodeDistance: calculateDistance,
-    //5 parameters, (city1, city2, optional:units, optional:stateForCity1, optional:stateForCity2)
-    cityDistance: getAllCityDistances,
-    //2 parameters, (type, query), type 'zipcode' or 'city', query zipcode or city name
-    getInfo: getInfo,
-}
-
 //console.log(zipCodeData.zipCodeDistance('98006', '33014'));
 //console.log(zipCodeData.cityDistance('miami lakes', 'miami'));
 //console.log(zipCodeData.cityDistance('miami', 'bellevue', 'M', 'FL', 'WA'));
 //console.log(zipCodeData.getInfo('zipcode', '98006'));
 
-exports.zipCodeData;
+//3 parameters: (zipcode1, zipcode2, optional:units), valid units: 'M','K','N' 
+exports.zipCodeDistance = calculateDistance;
+
+//5 parameters, (city1, city2, optional:units, optional:stateForCity1, optional:stateForCity2)
+exports.cityDistance = getAllCityDistances;
+
+//2 parameters, (type, query), type 'zipcode' or 'city', query zipcode or city name
+exports.getInfo = getInfo;
